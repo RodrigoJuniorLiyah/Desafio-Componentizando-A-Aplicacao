033c521e1c0eb82ee76f458ba3269c4f1467e1dd
Rodrigo Junior <Rodrigo.junior7@outlook.com>
2022-03-20 13:35:15 -0300

    correção na importação

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,4 +1,4 @@
-import { MovieCard } from "../components/MovieCard";
+import { MovieCard } from "./MovieCard";
 
 interface Props {
   selectedGenre: {
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,4 +1,4 @@
-import { Button } from "../components/Button";
+import { Button } from "./Button";
 
 interface Props {
   genresId: number;
