7a7f689c015417032bed03bb20128ab20b25aad3
Rodrigo Junior <Rodrigo.junior7@outlook.com>
2022-03-20 13:04:50 -0300

    Desafio concluído!

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,41 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "../components/Button";
+
+interface Props {
+  genresId: number;
+  genres: Genres[];
+  setGenreId: React.ComponentState;
+}
+
+interface Genres {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+export function SideBar(props: Props) {
+  const { genres, genresId: selectedGenreId, setGenreId: setSelectedGenreId } = props;
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre: Genres) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
